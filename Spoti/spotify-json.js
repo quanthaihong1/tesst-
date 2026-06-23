c1cac309f8600122f5e472e632eb606d96872a1b
th_wen.05 <83921790+quanthaihong1@users.noreply.github.com>
2026-06-23 23:57:47 +0700

    Change platform from 'iphone' to 'ipad' in URL

--- Spoti/spotify-json.js
+++ Spoti/spotify-json.js
@@ -1,7 +1,7 @@
 console.log(`spotify-json-2023.06.13`);
 let url = $request.url;
 if (url.includes('platform=iphone')) {
-    url = url.replace(/platform=iphone/, 'platform=iphone');
+    url = url.replace(/platform=iphone/, 'platform=ipad');
 } else {
     console.log('Không cần xử lý gì cả.');
 }
